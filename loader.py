71ffcdcbfe454ef904ea65b5d362db6395f02627
SebastianMCarreira <sebastian.m.careira@gmail.com>
2020-01-27 15:56:21 -0300

    Resume header and certifications

--- loader.py
+++ loader.py
@@ -3,7 +3,6 @@ from staticjinja import Site
 if __name__ == "__main__":
     site = Site.make_site(
         env_globals={
-            "static_host":".",
             "site_host":"."
         },
         searchpath="./templates",
