f81c8258c858ac14919a8b9b2906e69ba7dcf06e
SebastianMCarreira <sebastian.m.careira@gmail.com>
2020-01-27 20:16:31 -0300

    Portfolio

--- loader.py
+++ loader.py
@@ -3,7 +3,7 @@ from staticjinja import Site
 if __name__ == "__main__":
     site = Site.make_site(
         env_globals={
-            "site_host":"."
+            "site_host":"file:///C:/Users/Sebastián/Desktop/Coding/personal-web-out"
         },
         searchpath="./templates",
         outpath="../personal-web-out",
