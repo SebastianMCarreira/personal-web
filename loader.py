a84a8f8ab463503661833caa9158e1912381a5a4
SebastianMCarreira <sebastian.m.careira@gmail.com>
2020-01-28 08:10:10 -0300

    Site_host variable deleted

--- loader.py
+++ loader.py
@@ -1,14 +1,10 @@
 from staticjinja import Site
 import os
 
-SITE_HOST = os.environ.get("SITE_HOST",default=".")
 OUTPATH = os.environ.get("OUTPATH",default="../personal-web-out")
 
 if __name__ == "__main__":
     site = Site.make_site(
-        env_globals={
-            "site_host":SITE_HOST
-        },
         searchpath="./templates",
         outpath=OUTPATH,
         staticpaths=["static/"]
