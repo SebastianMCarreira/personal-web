5044354878284220c66e664fe61f89f9508f1461
SebastianMCarreira <sebastian.m.careira@gmail.com>
2020-01-28 19:44:42 -0300

    BuildSpec and appversion

--- loader.py
+++ loader.py
@@ -2,9 +2,13 @@ from staticjinja import Site
 import os
 
 OUTPATH = os.environ.get("OUTPATH",default="../personal-web-out")
+APPVERSION = os.environ.get("APPVERSION",default="testBuild")
 
 if __name__ == "__main__":
     site = Site.make_site(
+        env_globals={
+            "app_version"=APPVERSION
+        },
         searchpath="./templates",
         outpath=OUTPATH,
         staticpaths=["static/"]
