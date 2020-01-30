81de3b1d1dc3fbe6e22afa748d17b079b7cdbb66
SebastianMCarreira <sebastian.m.careira@gmail.com>
2020-01-29 22:38:18 -0300

    Abs path as environ variable

--- loader.py
+++ loader.py
@@ -3,11 +3,13 @@ import os
 
 OUTPATH = os.environ.get("OUTPATH",default="../personal-web-out")
 APPVERSION = os.environ.get("APPVERSION",default="testBuild")
+ISLOCALDEV = os.environ.get("ISLOCALDEV", default=True)
 
 if __name__ == "__main__":
     site = Site.make_site(
         env_globals={
-            "app_version":APPVERSION
+            "app_version":APPVERSION,
+            "abs_path": os.path.abspath(OUTPATH) if ISLOCALDEV else ""
         },
         searchpath="./templates",
         outpath=OUTPATH,
