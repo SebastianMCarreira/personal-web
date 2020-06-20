72678c26bd17cc1ab3df44d5e642296e5c8e3315
Sebastian Carreira <sebastian.m.carreira@gmail.com>
2020-06-20 00:20:54 -0300

    Added CSS compression

--- loader.py
+++ loader.py
@@ -1,8 +1,10 @@
 from staticjinja import Site
+from csscompressor import compress
 import os
 
 OUTPATH = os.environ.get("OUTPATH",default="/var/www/html")
 APPVERSION = os.environ.get("APPVERSION",default="testBuild")
+STATIC_PATH = os.environ.get("STATIC_PATH",default="static/")
 
 if __name__ == "__main__":
     site = Site.make_site(
@@ -11,6 +13,14 @@ if __name__ == "__main__":
         },
         searchpath="./templates",
         outpath=OUTPATH,
-        staticpaths=["static/"]
+        staticpaths=[STATIC_PATH]
     )
     site.render()
+
+    # css compression
+    print("Compressiong CSS files.")
+    for css_file in os.listdir(os.path.join(OUTPATH,STATIC_PATH,"css")):
+        with open(os.path.join(OUTPATH,STATIC_PATH,"css",css_file) ,"r") as f:
+            original_content = f.read()
+        with open(os.path.join(OUTPATH,STATIC_PATH,"css",css_file) ,"w") as f:
+            f.write(compress(original_content))
